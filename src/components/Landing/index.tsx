dbc2ff4e86f721b369fd186d0f7001d0f8c7d9a3
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2023-04-02 17:36:07 +0100

    aya community testing

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import {
-  useContractWrite,
-  useWaitForTransaction,
-} from "wagmi";
+import { useAccount, useContractWrite, useWaitForTransaction } from "wagmi";
 import { ToastContainer, toast } from "react-toastify";
 import { useRouter } from "next/router";
 import "react-toastify/dist/ReactToastify.css";
-
+import { Biconomy } from "@biconomy/mexa";
+import { ExternalProvider } from "@ethersproject/providers";
+import soulboundAbi from "../../../provider/abi/soulbound.json";
+import { ethers } from "ethers";
 function Landing() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const router = useRouter();
+  const soulbound = "0x21b02720bb2d06dd0e1cff7756137535410fae67";
+  const { address } = useAccount();
   const { data, write } = useContractWrite({
     mode: "recklesslyUnprepared",
-    address: "0x3c3079aB5510148E241e2277F46800a8389e60b7",
+    address: "0x21b02720bb2d06dd0e1cff7756137535410fae67",
     abi: [
       {
         inputs: [],
@@ -24,7 +28,7 @@ function Landing() {
     ],
     functionName: "certMint",
     onError(err: any) {
-      console.log("error:",err?.data)
+      console.log("error:", err?.data);
       toast.error(err?.data.message);
     },
   });
@@ -40,39 +44,116 @@ function Landing() {
     },
   });
 
+  
+
+  async function handleGaslessTxn() {
+    try {
+      setLoading(true);
+      if (typeof window.ethereum !== "undefined") {
+        const biconomy = new Biconomy(window.ethereum as ExternalProvider, {
+          apiKey: process.env.NEXT_PUBLIC_BICONOMY_API_KEY as string,
+          debug: true,
+          contractAddresses: [soulbound],
+        });
+        const provider = await biconomy.provider;
+
+        const contractInstance = new ethers.Contract(
+          soulbound,
+          soulboundAbi,
+          biconomy.ethersProvider
+        );
+        await biconomy.init();
+
+        const { data } = await contractInstance.populateTransaction.certMint();
+
+        let txParams = {
+          data: data,
+          to: soulbound,
+          from: address,
+          signatureType: "EIP712_SIGN",
+        };
+        // @ts-ignore
+        const tx: any = await provider.send("eth_sendTransaction", [txParams]);
+        if (tx.name == "Error") {
+          toast.error(tx.reason);
+        } 
+        biconomy.on(
+          "txMined",
+          (data: {
+            msg: string;
+            id: string;
+            hash: string;
+            receipt: string;
+          }) => {
+            setLoading(false);
+            toast.success("Successfully minted");
+            window.open("https://opensea.io/account", "_blank");
+          }
+        );
+        setLoading(false);
+      }
+    } catch (error: any) {
+      setLoading(false);
+      setError(
+        error.message || "An error occurred while processing your request."
+      );
+    }
+  }
+
+  const handleTnx = async () => {
+    try {
+      const userProvider = new ethers.providers.Web3Provider(
+        window.ethereum as ExternalProvider
+      );
+
+      const signer = userProvider.getSigner();
+      const from = await signer.getAddress();
+      const balance = await userProvider.getBalance(from);
+
+      const canSendTx = balance.gt(1e15);
+      if (canSendTx) {
+        return write?.();
+      } else {
+        return handleGaslessTxn();
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
   return (
     <>
-     <ToastContainer />
-    <section className="container mx-auto">
-      <div className="grid items-center md:grid-cols-2 md:gap-x-12 lg:grid-cols-2 lg:gap-x-12 mb-24">
-        <div className="p-2 md:p-6 lg:p-0 text-center md:text-left lg:text-left">
-          <h2 className="text-gray-200 md:text-medium md:leading-medium lg:text-large lg:leading-large font-black">
-            Grab your
-            <br /> Soul-bound certificate{" "}
-          </h2>
-          <p className="text-gray-200 md:text-xs lg:text-small lg:leading-small my-4">
-            This is only open to the approved addresses and
-            <br /> can’t be transferred out once minted.
-          </p>
-          <button
-            disabled={isLoading}
-            onClick={() => write?.()}
-            className="bg-[#3BD6B2] text-gray-200  px-24 py-4 rounded-full"
-          >
-            {isLoading ? "Minting" : "Mint Certificate"}
-          </button>
-        </div>
+      <ToastContainer />
+      <section className="container mx-auto">
+        <div className="grid items-center md:grid-cols-2 md:gap-x-12 lg:grid-cols-2 lg:gap-x-12 mb-24">
+          <div className="p-2 md:p-6 lg:p-0 text-center md:text-left lg:text-left">
+            <h2 className="text-gray-200 md:text-medium md:leading-medium lg:text-large lg:leading-large font-black">
+              Grab your
+              <br /> Soul-bound certificate{" "}
+            </h2>
+            <p className="text-gray-200 md:text-xs lg:text-small lg:leading-small my-4">
+              This is only open to the approved addresses and
+              <br /> can’t be transferred out once minted.
+            </p>
+            <button
+              disabled={isLoading || loading}
+              onClick={handleTnx}
+              className="bg-[#3BD6B2] text-gray-200  px-24 py-4 rounded-full"
+            >
+              {isLoading || loading ? "Minting" : "Mint Certificate"}
+            </button>
+          </div>
 
-        <div>
-          <Image
-            src="/desktop.png"
-            width={539}
-            height={573}
-            alt="desktop__svg"
-          />
+          <div>
+            <Image
+              src="/desktop.png"
+              width={539}
+              height={573}
+              alt="desktop__svg"
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 }
